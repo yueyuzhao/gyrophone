3a7f63d8c8b82427b610301d746a79e062cdc2cf
Yan Michalevsky <yanm2@stanford.edu>
2013-09-15 21:44:30 -0700

    Add optional filtering to plot_spectrums

--- plot_spectrums.m
+++ plot_spectrums.m
@@ -1,6 +1,9 @@
-function plot_spectrums(filename, fs)
-    nfft = 256;
+function plot_spectrums(filename, fs, flt)
+    nfft = 64;
     [~, samples] = read_samples_file(filename);
+    if exist('flt', 'var')
+        samples = flt(samples);
+    end;
     for k = 1:3
         figure;
         % specgram(samples(:, k), 128, 120);
