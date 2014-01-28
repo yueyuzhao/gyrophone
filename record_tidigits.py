bbb764196aeaac0cc8c0c3bb2b8c8bc0b25590a3
Yan Michalevsky <yanm2@stanford.edu>
2014-01-27 16:50:47 -0800

    single-digit option added

--- record_tidigits.py
+++ record_tidigits.py
@@ -4,9 +4,9 @@ from optparse import OptionParser
 import run_gyromic
 import os, os.path
 
-WAV_EXT = '.WAV'
+WAV_EXT = '.wav'
 
-def process(input_dir, output_dir, dry):
+def process(input_dir, output_dir, dry, single_digit):
 	for root, dirs, files in os.walk(input_dir):
 		print 'Processing', root
 		dest_dir = os.path.join(output_dir, *root.split(input_dir)[1:])
@@ -14,9 +14,10 @@ def process(input_dir, output_dir, dry):
 		if not dry:
 			if not os.path.exists(dest_dir):
 				os.makedirs(dest_dir)
-		wavfiles = [f for f in files if os.path.splitext(f)[1] == WAV_EXT]
-		single_digit = [f for f in wavfiles if len(os.path.splitext(f)[0]) == 2]
-		for f in single_digit:
+		wavfiles = [f for f in files if os.path.splitext(f)[1].lower() == WAV_EXT]
+		if single_digit:
+			wavfiles = [f for f in wavfiles if len(os.path.splitext(f)[0]) == 2]
+		for f in wavfiles:
 			print 'Recording', f	
 			if not dry:
 				run_gyromic.play_and_record(os.path.join(root, f),
@@ -27,13 +28,15 @@ def main():
 	parser = OptionParser("Usage: %prog [--dry] <input directory> <output_directory>")
 	parser.add_option('--dry', action="store_true", dest="dry", default=False, 
 										help="Dry run (don't record)")
+	parser.add_option('--single-digit', action="store_true", dest="single_digit", 
+										default=False, help="Playback only single digit files from TIDIGITS")
 	(options, args) = parser.parse_args()
 	if len(args) < 2:
 		parser.error('Not enough arguments. Specify input and output directories.')
 	
 	input_dir = args[0]
 	output_dir = args[1]
-	process(input_dir, output_dir, options.dry)
+	process(input_dir, output_dir, options.dry, options.single_digit)
 
 if __name__ == '__main__':
 	main()
