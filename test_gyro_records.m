560f2b49b99a788be19d2277880e2595d8b5d12f
Yan Michalevsky <yanm2@stanford.edu>
2014-02-13 17:12:20 -0800

    Changes to test_gyro_records

--- test_gyro_records.m
+++ test_gyro_records.m
@@ -1,30 +1,44 @@
-function [timestamps, gyro, gyro_merged] = test_gyro_records
+function [timestamps, gyro, reconstructed] = ...
+    test_gyro_records(sample_name, ref_signal_name)
 % Test with multiple Gyro samples of a signal
-fs = 8000;
+
+GYRO_REC_DIR = 'gyro_results/Nexus';
 
 global GYRO_FS;
 GYRO_FS = 200;
-n = floor(fs / GYRO_FS);
+% n = floor(fs / GYRO_FS);
 
-ref_signal_name = 'ref_signal';
-sample_name = 'triang-50-100_100-200hz';
+% ref_signal_name = 'ref_signal';
+% sample_name = 'triang-50-100_100-200hz';
 DEVICES = {'00a697fa469633a5', '0094e779d7d1841f', '015d3fb673180c13', '04dc22d4dad7e4ce'};
 NUM_DEVICES = 2; % length(DEVICES);
+fs = NUM_DEVICES * GYRO_FS;
 
-dim = 2;
+REFINE_OFFSET = true;
+REFINE_TIMESKEW = true;
+USE_APPROX_OFFSET = false;
+
+dim = 1; % Gyro dimension to use
 
 timestamps = cell(NUM_DEVICES, 1);
+
 gyro = cell(NUM_DEVICES, 1);
-offset = zeros(NUM_DEVICES, 1);
+offset = zeros(1, NUM_DEVICES);
 
-[~, ~, ref_signal_ds] = read_ref_signal(ref_signal_name);
+% We resample the reference signal to 1xGYRO_FS since this is the 
+% effective frequency a single devices can capture
+[~, ~, ref_signal_ds] = read_signal(ref_signal_name, 1);
+ref_signal_fs = GYRO_FS;
 
-%% Find the offset at which the reference signal appears in
-% each recording
+original_signal_name = sample_name;
+[~, ~, orig_sig_ds] = read_signal(original_signal_name, NUM_DEVICES);
+
+%% Read recorded samples
 all_rec_plot = figure;
-hold all; 
+title('All recordings');
+hold all;
 for i = 1:NUM_DEVICES
-    filename = ['gyro_results/Nexus/' DEVICES{i} '/' sample_name];
+    filename = [GYRO_REC_DIR '/' DEVICES{i} '/' sample_name];
     [timestamps{i}, gyro{i}] = read_samples_file(filename);
     display([num2str(length(gyro{i})) ' samples in Gyro ' num2str(i) ' recording']);
     gyro{i} = normalization( gyro{i}(:, dim) );
@@ -33,96 +47,49 @@ for i = 1:NUM_DEVICES
     title(['Gyro ' num2str(i) ' samples']);
     figure(all_rec_plot);
     plot(gyro{i});
-    offset(i) = find_offset(gyro{i}, ref_signal_ds);
+end
+legend(DEVICES{1:NUM_DEVICES});
+
+%% Find approximate offset based on timestamps
+approximate_offset = get_approximate_offset(timestamps, GYRO_FS)';
+display(approximate_offset);
+
+if USE_APPROX_OFFSET
+    offset = approximate_offset;
+else
+    %% Find the offset at which the reference signal appears in
+    % each recording
+    for i = 1:NUM_DEVICES
+        offset(i) = find_offset(gyro{i}, GYRO_FS, ref_signal_ds, ref_signal_fs);
+    end
+    % set minimal offset to 0 - prevent negative offsets
+    offset = offset - min(offset);
+    display(offset);
 end
 
-legend(DEVICES{1:NUM_DEVICES});
-display(offset);
+if REFINE_OFFSET
+    offset = refine_offset(fs, offset, 10, NUM_DEVICES, gyro, orig_sig_ds);
+end
 
-original_signal_name = 'triang-50-100_100-200hz';
-[~, ~, orig_sig_ds] = read_ref_signal(original_signal_name);
+time_skew = offset_to_timeskew(offset, NUM_DEVICES, ref_signal_fs);
+trimmed = trim_signals(gyro, offset);
 
-% find the shift in offset for which we get the 
-% maximum correlation with the original signal
-MAX_SHIFT = 10;
-shift_range = -MAX_SHIFT:MAX_SHIFT;
-score = zeros(length(shift_range));
-progressbar('Finding maximum correlation...');
-for d1 = shift_range
-    for d2 = shift_range
-        shift_offset = [d1; d2];
-        [~, gyro_merged, ~] = ...
-            merge_sensor_data(timestamps, gyro, offset + shift_offset, GYRO_FS);
-        gyro_merged = normalization(gyro_merged);
-        max_corr = max(xcorr(gyro_merged, orig_sig_ds));
-        score(d1 + MAX_SHIFT + 1, d2 + MAX_SHIFT + 1) = max_corr;
-        progressbar(((d1 + MAX_SHIFT) * length(shift_range) + (d2 + MAX_SHIFT)) / length(score(:)));
-    end;
-end;
-progressbar(1); % close progress bar
+if REFINE_TIMESKEW
+    time_skew = refine_timeskew(time_skew, NUM_DEVICES, trimmed, orig_sig_ds);
+end
 
-% pick best score and merge according to the corresponding offset shift
-[~, max_score_ind] = max(score(:));
-display(max_score_ind);
-[d1, d2] = ind2sub(size(score), max_score_ind);
-offset_shift = [d1; d2] - [MAX_SHIFT+1; MAX_SHIFT+1];
-display(offset_shift);
-[~, gyro_merged, merged_fs] = ...
-            merge_sensor_data(timestamps, gyro, offset + offset_shift, GYRO_FS);
-gyro_merged = normalization(gyro_merged);
-
+[reconstructed, reconstructed_fs] = eldar_reconstruction(GYRO_FS, trimmed, time_skew);
 
 figure;
-fft_plot(gyro_merged, merged_fs);
+fft_plot(reconstructed, reconstructed_fs);
 title('Merged from Gyro recordings');
+playsound(reconstructed, reconstructed_fs);
 
-gyro_resampled = resample(gyro_merged, fs, merged_fs);
-% figure;
-% subplot(121);
-% fft_plot(gyro_resampled, fs);
-% title('FFT After resampling');
-
-soundsc(gyro_resampled, fs);
-
-% filtered = bp_100_200hz(gyro_resampled);
+% bp = fir1(48, [0.1 0.8]);
+% filtered = filter(bp, 1, reconstructed);
 % figure;
 % fft_plot(filtered, fs);
 % title('Filtered low frequencies');
+% playsound(filtered, reconstructed_fs);
 
-% soundsc(filtered, fs);
-
-end
-
-function [merged_timestamps, merged_samples, merged_fs] = ...
-    merge_sensor_data(timestamps, samples, offsets, fs)
-    
-    num_devices = length(samples);
-
-    % trim recordings according to offsets
-    % figure; hold all;
-    for i = 1:num_devices
-        trimmed_timestamps{i} = timestamps{i}(offsets(i):end);
-        trimmed_samples{i} = samples{i}(offsets(i):end);
-        % plot(trimmed_samples{i});
-    end
-    title('Trimmed signals');
-    
-    merged_timestamps = interleave_vectors(trimmed_timestamps);
-    merged_samples = interleave_vectors(trimmed_samples);
-    merged_fs = fs * num_devices;
-    % [~, gyro_merged] = interpolate_samples(merged_timestamps, gyro_merged);
-end
-
-function [ref_signal, refsig_fs, ref_signal_ds] = ...
-    read_ref_signal(sample_name)
-global GYRO_FS;
-[ref_signal, refsig_fs] = audioread(['samples/' sample_name '.wav']);
-ref_signal_ds = resample(ref_signal, GYRO_FS * 2, refsig_fs);
-ref_signal_ds = normalization(ref_signal_ds);
-display(['Samples in ' sample_name ' (resampled): ' ...
-    num2str(length(ref_signal_ds))]);
-figure;
-subplot(121);
-fft_plot(ref_signal_ds, GYRO_FS * 2);
-title('Reference signal FFT');
 end
