75cae79a54889dc586dcd5a6ac4722f4e8f256df
Yan Michalevsky <yanm2@stanford.edu>
2014-02-27 04:09:23 -0800

    Following derivation in paper

--- eldar_reconstruction.m
+++ eldar_reconstruction.m
@@ -3,8 +3,8 @@ function [output, fs] = eldar_reconstruction(inp_fs, input, time_skew)
     
     N = length(time_skew); % number of samplers
     fs = inp_fs * N; % full sampling frequency
-    T_Q = 1 / fs;
-    T = N * T_Q;
+    T_Q = 1 / fs; % Nyquist sampling period
+    T = N * T_Q; % single sampler period
     
     % index_mapping is used to reorded filters in case the first input 
     % signal is actually delayed comparing to the second and not vice versa
@@ -14,6 +14,7 @@ function [output, fs] = eldar_reconstruction(inp_fs, input, time_skew)
     
     filtered = cell(N, 1);
     for i = 1:N
+        % upsampling - according to the interpolation identity
         upsampled_input = upsample(input{index_mapping(i)}, N);
         filtered{i} = filter(H{i}, 1, upsampled_input);
     end
@@ -38,30 +39,36 @@ function filter_value = get_filter_value(N, T, a, time_skew, p, t)
        sine_product_elements(q+1) = sin(pi*(t + tp - tq)/T);
     end
     sine_product = prod(sine_product_elements);
-    filter_value = a(p + 1) * sinc(t/T) * sine_product;
+    filter_value = a(p + 1) * sinc((t)/T) * sine_product;
 end
 
-function H = construct_filters(N, T, time_skew)
-    a = zeros(N, 1);
-    for p = 0:N-1
-        tp = time_skew(p+1);
-        tq = time_skew;
-        tq(tq == tp) = [];
-        a(p+1) = 1 / prod(sin(pi*(tp - tq)/T));
-    end
+function H = construct_filters(N, T, tau)
+    a = build_coefficients(N, T, tau);
     
     TAPS = 48;
     Tq = T/N;
     H = cell(N, 1);
     for p = 0:N-1
         H{p+1} = zeros(TAPS, 1);
-        tp = time_skew(p+1);
+        tp = tau(p+1);
         for n = 0:TAPS-1
             t = (n*Tq - tp);
-            H{p+1}(n+1) = get_filter_value(N, T, a, time_skew, p, t);            
+            H{p+1}(n+1) = get_filter_value(N, T, a, tau, p, t);            
         end
         
 %         H{p+1}(tp/Tq+1) = 1;
-%         fvtool(H{p+1});
+        fvtool(H{p+1});
+    end
+end
+
+function a = build_coefficients(N, T, tau)
+    % compute a coefficients according to Sindhi-Prabhu
+    a = ones(N, 1);
+    for p = 1:N
+        for q = 1:N
+            if q ~= p
+                a(p) = a(p) / sin(pi*(tau(p) - tau(q)) / T);
+            end
+        end
     end
 end
