38e3fa7a9430dde0296ba659eff22d77c55b9552
Yan Michalevsky <yanm2@stanford.edu>
2014-05-25 22:05:37 -0700

    Remove debug output

--- trim_signals.m
+++ trim_signals.m
@@ -3,10 +3,17 @@ function output = trim_signals(input, offset)
     N = length(offset);
     output = cell(size(input));
     frame_index = floor(offset/N);
-    display(frame_index);
+%     display(frame_index);
     trim_index = max(frame_index) - frame_index;
-    display(trim_index);
+%     display(trim_index);
     for i=1:N
         output{i} = input{i}((trim_index(i)+1):end);
+    end
+    
+    % trim end so all signals have same length
+    % required by non-uniform reconstruction algorithm
+    minlen = min(cellfun('length', output));
+    for i = 1:N
+       output{i} = output{i}(1:minlen); 
     end
 end
